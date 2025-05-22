140f57f44567a13f7fc686f0acc31021c79b0eb5
MARKANTONY04 <markmarsden04@gmail.com>
2025-05-22 20:53:13 +0100

    Changed database URL in env.py, migrated, created a new super user, and loaded data from json file to transfer posts to blog.

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_Bg8FJobrlP7H@ep-rapid-rice-a2jm5w8z.eu-central-1.aws.neon.tech/decoy_civil_chomp_48700")
+    "DATABASE_URL", "postgresql://neondb_owner:npg_Bg8FJobrlP7H@ep-rapid-rice-a2jm5w8z.eu-central-1.aws.neon.tech/self_spray_user_219813")
 os.environ.setdefault("SECRET_KEY", "django-insecure-x+_04zqv8x!os6xgmz3$8mytkr6(%j(m4h^5l4hylo&-x0o#@j")
 
